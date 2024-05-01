612df38318024596e826e5846986349cb707b7e4
Asher_Yang <a0009751@gmail.com>
2024-05-01 16:20:51 +0800

    Update date.h

--- backend/date.h
+++ backend/date.h
@@ -16,9 +16,17 @@ using namespace std;
 #include <ctime>
 #include <ratio>
 
-/* getdayOfTrip
+/* function : getdayOfTrip
  *
  * Description : return how many day between the two date
+ *
+ * value : 
+ * syear (start year)
+ * smonth (start month)
+ * sday (start day)
+ * eyear (end year)
+ * emonth (end month)
+ * eday (end day)
  *
  * */
 int getdayOfTrip(int syear, int smonth, int sday,
@@ -49,9 +57,14 @@ int getdayOfTrip(int syear, int smonth, int sday,
 
 /* getStartAndEndDay(string year, string month, string day)
  *
- * Description : generate the date (4 group)
+ * Description : generate the date interval (5 group, include type date)
  *
- *
+ * Example : input (2024-05-01(start date) -> 2024-05-03(end day))
+ *           output  2024-05-01 -> 2024-05-03
+ *                   2024-05-08 -> 2024-05-10
+ *                   2024-05-15 -> 2024-05-17
+ *                   2024-05-22 -> 2024-05-24
+ *                   2024-05-29 -> 2024-05-31
  * */
 vector<pair<string,string>> getStartAndEndDay(string Startyear, string Startmonth, string Startday,
 											  string Endyear, string Endmonth, string Endday){
@@ -68,7 +81,7 @@ vector<pair<string,string>> getStartAndEndDay(string Startyear, string Startmont
 
 	//get how many day of trip
 	int dayOfTrip = getdayOfTrip(syear,smonth,sday,eyear,emonth,eday);
-
+    //set the started day
     std::tm start_date = {};
     start_date.tm_year = syear - 1900; //
     start_date.tm_mon = smonth-1; // 5
@@ -77,18 +90,20 @@ vector<pair<string,string>> getStartAndEndDay(string Startyear, string Startmont
     std::chrono::system_clock::time_point start_tp = std::chrono::system_clock::from_time_t(std::mktime(&start_date));
 
     for (int i = 1; i < 5; ++i) {
+	// next start day is next week
         std::chrono::system_clock::time_point next_week = start_tp + std::chrono::hours(24 * 7 * i);
 
         std::time_t next_week_time = std::chrono::system_clock::to_time_t(next_week);
         std::tm* next_week_tm = std::localtime(&next_week_time);
 
+	//put next start day in to vector
         ans[i].first = to_string(next_week_tm->tm_year + 1900)+"-"+to_string(next_week_tm->tm_mon + 1)+"-"+to_string(next_week_tm->tm_mday);
-
+	//calaulate next end day by dayOfTrip
         std::chrono::system_clock::time_point next_week_end = next_week + std::chrono::hours(24 * dayOfTrip);
 
         std::time_t next_week_end_time = std::chrono::system_clock::to_time_t(next_week_end);
         std::tm* next_week_end_tm = std::localtime(&next_week_end_time);
-
+	// put next end day into vector
         ans[i].second = to_string(next_week_end_tm->tm_year + 1900)+"-"+to_string(next_week_end_tm->tm_mon + 1)+"-"+to_string(next_week_end_tm->tm_mday);
     }
 
